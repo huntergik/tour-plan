2dbc411add211e74a294015789af2b431195dd78
huntergik <oleg.engelgart@yandex.ru>
2020-09-23 17:26:09 +0600

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    prevEl: ".slider-button--prev",
-    nextEl: ".slider-button--next",
+    prevEl: ".hotel-slider__button--prev",
+    nextEl: ".hotel-slider__button--next",
   },
   // effect: "cube",
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: ".reviews-slider__button--prev",
+    nextEl: ".reviews-slider__button--next",
+  },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
