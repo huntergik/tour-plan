bb1990bcccd8922ad36544a1a35ca8520a1c9de0
huntergik <oleg.engelgart@yandex.ru>
2020-10-01 21:21:23 +0600

    fix; SEND.PHP

--- send.php
+++ send.php
@@ -16,7 +16,7 @@ $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
+<b>Сообщение:</b><br>$message<br><br>
 <b>Ваш почтовый ящик:</b><br>$email
 ";
 
